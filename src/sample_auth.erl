88777e026dc785e764d4b7100ff7e7e9a8c6d266
waheguru <waheguru@waheguru-Vostro-2520.(none)>
2015-01-23 10:45:39 +0530

    working with post method

--- src/sample_auth.erl
+++ src/sample_auth.erl
@@ -1,38 +1,43 @@
 -module(sample_auth).
 -export([init/3]).
 
--export([content_types_provided/2]).
+% -export([content_types_provided/2]).
 % -export([]).
 -export([welcome/2, terminate/3, allowed_methods/2]).
-% -export([]).
+-export([content_types_accepted/2]).
 
 %% Init
 init(_Transport, _Req, []) ->
 	{upgrade, protocol, cowboy_rest}.
 
 %% Callbacks
-  allowed_methods(Req, State) ->  
+allowed_methods(Req, State) ->  
     {[<<"GET">>, <<"POST">>], Req, State}.  
 
-% content_types_accepted(Req, State) ->
-% {[{{<<"application">>, <<"json">>, []}, welcome}], Req, State}.
-content_types_provided(Req, State) ->
-	{[		
-		{<<"text/html">>, welcome}	
-	], Req, State}.
+content_types_accepted(Req, State) ->
+{[{{<<"application">>, <<"json">>, []}, welcome}], Req, State}.
+% content_types_provided(Req, State) ->
+% 	{[		
+% 		{<<"text/html">>, welcome}	
+% 	], Req, State}.
 
 terminate(_Reason, _Req, _State) ->
 	ok.
 
 %% API
+% welcome(Req, State) ->
 welcome(Req, State) ->
-io:format("into Welcome definition ************************************"),
-	{Method, Req2} = cowboy_req:method(Req),
-		io:format("Method is ~p ~n ", [Method]),
-		io:format("Req2 is ~p ~n ", [Req2]),
-		Req1 = cowboy_req:method(Req),
+io:format("Initial Req from the request  is ~p ~n", [Req]),
+io:format("Initial State from the request is ~p ~n", [State]),
+	Req_method = cowboy_req:method(Req),
+	io:format("Req_method is ~p ~n", [Req_method]),
+	Req_Body = cowboy_req:body(Req),
+	io:format("Body is ~p ~n", [Req_Body]),
+	Body = <<"<h1>This is a response for other methods</h1>">>,
+	io:format("Body is  ~p ~n",[Body]),
+	{Body, Req, State}.
      % Req1 = cowboy_http_req:body(Req),
-	io:format("Request is ~p ~n", [Req1]),
+	% io:format("Request is ~p ~n", [Req1]),
 	% io:format("password ~p ~n",[Req]),
 	% compile:file("/home/waheguru/erlang-projects/contentapi/src/mysql.erl"),
 	% compile:file("/home/waheguru/erlang-projects/contentapi/src/mysql_conn.erl"),
@@ -57,12 +62,10 @@ io:format("into Welcome definition ************************************"),
 
 	% io:format("password ~p ~n",[Password]),
 
-	{Username, _} =  cowboy_req:qs_val(<<"Username">>, Req),
-	{Password, _} =  cowboy_req:qs_val(<<"Password">>, Req),
-	{Namespace, _} = cowboy_req:qs_val(<<"Namespace">>, Req),
+	% {Username, _} =  cowboy_req:qs_val(<<"Username">>, Req),
+	% {Password, _} =  cowboy_req:qs_val(<<"Password">>, Req),
+	% {Namespace, _} = cowboy_req:qs_val(<<"Namespace">>, Req),
 
-	Body = "{ 'username':'"++binary_to_list(Username)++"'},{ 'password':'"++binary_to_list(Password)++"'},{ 'namespace':'"++binary_to_list(Namespace)++"'}",
-	io:format("Body is  ~p ~n",[Body]),
-    {Body, Req, State}.
-
-    
+	% Body = "{ 'username':'"++binary_to_list(Username)++"'},{ 'password':'"++binary_to_list(Password)++"'},{ 'namespace':'"++binary_to_list(Namespace)++"'}",
+	% io:format("Body is  ~p ~n",[Body]),
+    % {Body, Req, State}.
